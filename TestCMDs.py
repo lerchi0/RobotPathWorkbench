6003a4746d49e51ae44e523a1f44431c581f658c
Thomas <tle3105@students.fhv.at>
2021-03-25 21:31:45 +0100

    update

--- TestCMDs.py
+++ TestCMDs.py
@@ -4,6 +4,7 @@ import FreeCAD as App
 
 import RPWlib
 
+import Part,PartGui
 
 
 class TestCMD_1():
@@ -29,7 +30,16 @@ class TestCMD_1():
         except Exception:
             element_ = ""
 
+        doc = App.activeDocument()
+        try:
+            doc.removeObject("Shape")
+        except:
+            App.Console.PrintMessage("no previous Center-Sphere found")
+            App.Console.PrintMessage("\r\n")
+
+
         App.Console.PrintMessage("Type: {}\r\n".format(element_.ShapeType))
+
 
         if (element_.ShapeType == "Vertex"):
             pos = element_.Point
@@ -37,6 +47,8 @@ class TestCMD_1():
             App.Console.PrintMessage("y = {}\r\n".format(pos[1]))
             App.Console.PrintMessage("z = {}\r\n".format(pos[2]))
             App.Console.PrintMessage("\r\n")
+            s = Part.makeSphere(0.5,pos)
+            Part.show(s)
         elif (element_.ShapeType == "Face"):
             pos = element_.BoundBox.Center
             normalVect = element_.normalAt(0,0)
@@ -45,22 +57,27 @@ class TestCMD_1():
             App.Console.PrintMessage("z = {}\r\n".format(pos[2]))
             App.Console.PrintMessage("normal Vector = {}\r\n".format(normalVect))
             App.Console.PrintMessage("\r\n")
+            s = Part.makeSphere(0.5,pos)
+            Part.show(s)
         elif (element_.ShapeType == "Edge"):
             startPoint = element_.Vertexes[0]
             endPoint = element_.Vertexes[1]
             direction = endPoint.Point - startPoint.Point
+            pos = startPoint.Point + 0.5*direction
             App.Console.PrintMessage("start = {}\r\n".format(startPoint.Point))
             App.Console.PrintMessage("end = {}\r\n".format(endPoint.Point))
+            App.Console.PrintMessage("center = {}\r\n".format(pos))
             App.Console.PrintMessage("direction = {}\r\n".format(direction))
             App.Console.PrintMessage("\r\n")
-
+            s = Part.makeSphere(0.5,pos)
+            Part.show(s)
 
 class TestCMD_1_CMD():
     """My new command"""
 
     def GetResources(self):
         return {'Pixmap'  :  RPWlib.pathOfModule() + "/icons/WB_linSegCMD_2_icon.svg", # the name of a svg file available in the resources
-                'Accel' : "Alt+P", # a default shortcut (optional)
+                'Accel' : "", # a default shortcut (optional)
                 'MenuText': "Print Center Point",
                 'ToolTip' : "Print Selected Object Center Point"}
 
